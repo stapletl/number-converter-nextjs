e58332d2d7a258004d7afcc775b065f72fb61b39
Logan Stapleton <lojostapleton@gmail.com>
2024-03-10 12:13:27 -0400

    Wireframe in place

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,51 @@
+import { ModeToggle } from "@/components/ui/ModeToggle";
 import {
   Card,
   CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import Image from "next/image";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
-export default function Home() {
+const Home = () => {
+
   return (
-    <div className="w-full h-[100vh] flex items-center justify-center">
-    <Card>
-      <CardHeader>
-        <CardTitle>Card Title</CardTitle>
-        <CardDescription>Card Description</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <p>Card Content</p>
-      </CardContent>
-      <CardFooter>
-        <p>Card Footer</p>
-      </CardFooter>
-    </Card>
-
+    <div>
+      <div className="flex justify-end p-4">
+        <ModeToggle />
+      </div>
+      <div className="w-full h-full flex items-center justify-center">
+        <Card>
+          <CardHeader>
+            <CardTitle>Number Converter</CardTitle>
+            <CardDescription>
+              Convert between different number bases in real time
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <div className="pb-1.5 grid w-full max-w-sm items-center gap-1.5">
+              <Label htmlFor="decimal">Decimal</Label>
+              <Input type="text" id="decimal" placeholder="1234" />
+            </div>
+            <div className="pb-1.5 grid w-full max-w-sm items-center gap-1.5">
+              <Label htmlFor="binary">Binary</Label>
+              <Input type="text" id="binary" placeholder="10011010010" />
+            </div>
+            <div className="pb-1.5 grid w-full max-w-sm items-center gap-1.5">
+              <Label htmlFor="hex">Hex</Label>
+              <Input type="text" id="hex" placeholder="4d2" />
+            </div>
+            <div className="grid w-full max-w-sm items-center gap-1.5">
+              <Label htmlFor="octal">octal</Label>
+              <Input type="text" id="octal" placeholder="2322" />
+            </div>
+          </CardContent>
+        </Card>
+      </div>
     </div>
   );
 }
+
+export default Home;
